637abb99792b9a4f02d69e9e3a12afcea5032562
MD ARAFATH HOSSAIN SHEMANTO <48240273+mdarafath03@users.noreply.github.com>
2019-04-29 15:26:16 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,99 @@
+(function ($) {
+"use strict";
+
+
+
+// MOBILE MENU
+jQuery('#mobile-menu').meanmenu({
+	container: "header"
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:3
+        },
+        992:{
+            items:5
+        }
+    }
+})
+
+  // scrollIt for smoth scroll
+  $.scrollIt({
+    upKey: 38,             // key code to navigate to the next section
+    downKey: 40,           // key code to navigate to the previous section
+    easing: 'linear',      // the easing function for animation
+    scrollTime: 600,       // how long (in ms) the animation takes
+    activeClass: 'active', // class given to the active nav element
+    onPageChange: null,    // function(pageIndex) that is called when page is changed
+    topOffset: 0           // offste (in px) for fixed top navigation
+	});
+	
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: '.grid-item',
+	  }
+	});
+});
+
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});	
+
+	// Typed js for typing
+	var typed3 = new Typed('.typed', {
+		strings: ['web designer', 'web developer', 'photographer'],
+		typeSpeed: 100,
+		backSpeed: 30,
+		loop: true
+	});
+	// Typed js for typing
+	var typed3 = new Typed('.type', {
+		strings: ['web designer', 'web developer', 'photographer'],
+		typeSpeed: 100,
+		backSpeed: 30,
+		loop: true
+	});
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+
+})(jQuery);	
